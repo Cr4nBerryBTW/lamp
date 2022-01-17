4e67a9859ef6c22c3f549bccc62e8ab563634f62
denis <mixtapev11@gmail.com>
2022-01-17 14:39:49 +0300

    the end

--- app/index.php
+++ app/index.php
@@ -1,5 +1,15 @@
-<h1>Hello Cloudreach!</h1>
+<h1>Hello!</h1>
 <h4>Attempting MySQL connection from php...</h4>
 <?php
-phpinfo();
-?>
+$host = 'mysql';
+$user = 'root';
+$pass = 'rootpassword';
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected to MySQL successfully!";
+
+
+
